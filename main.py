dc7d7f38e689a7ac38c3a72f7928a1ce36c0287d
jfleck25 <jfleck25@gmail.com>
2026-03-30 17:08:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ else:
     print(" * Sentry DSN not found - error tracking disabled (set SENTRY_DSN environment variable)")
 
 # --- Configuration & Constants ---
-app = flask.Flask(__name__, static_folder='.', static_url_path='')
+app = flask.Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
 allowed_origins = os.environ.get("ALLOWED_ORIGINS")
